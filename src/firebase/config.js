b846664ba9f34fc853d7ad007545d054349aac79
Guptaabhi279 <abhishekgupta22021999@gmail.com>
2021-06-01 20:11:49 +0530

    updated

--- src/firebase/config.js
+++
@@ -1,7 +0,0 @@
-// firebase config
-export const firebaseConfig = {apiKey: "AIzaSyBeQt2awLfSC4W2lER5DDeLsilMjY6lG3Y",
-authDomain: "ecommerce-website-292b7.firebaseapp.com",
-projectId: "ecommerce-website-292b7",
-storageBucket: "ecommerce-website-292b7.appspot.com",
-messagingSenderId: "925750390358",
-appId: "1:925750390358:web:9c062fb3bd96fb30a595f7"};
--- src/stripe/config.js
+++
@@ -1,1 +0,0 @@
-export const publishableKey = 'pk_test_51IsUwRSHjqcyAYzTpnATFu7GN5ujjk5TsKlAnwXYbewTF92aIxG6tozgzR449GU03Y5YwqznwiUROhuDIh24RGHp008lVLpKd2';
